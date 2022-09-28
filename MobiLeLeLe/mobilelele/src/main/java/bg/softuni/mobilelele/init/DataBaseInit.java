60397d410c01c6f0c49c4c17c79be3e3d2aaf8f7
Teodora Uzunova <tedyuzunova@gmail.com>
2022-09-28 14:12:40 +0300

    test registration

--- MobiLeLeLe/mobilelele/src/main/java/bg/softuni/mobilelele/init/DataBaseInit.java
+++ MobiLeLeLe/mobilelele/src/main/java/bg/softuni/mobilelele/init/DataBaseInit.java
@@ -1,9 +1,6 @@
 package bg.softuni.mobilelele.init;
 
-import bg.softuni.mobilelele.service.BrandService;
-import bg.softuni.mobilelele.service.ModelService;
-import bg.softuni.mobilelele.service.OfferService;
-import bg.softuni.mobilelele.service.UserService;
+import bg.softuni.mobilelele.service.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,18 +11,21 @@ public class DataBaseInit implements CommandLineRunner {
     private final ModelService modelService;
     private final OfferService offerService;
     private final UserService userService;
+    private final UserRoleService userRoleService;
 
-    public DataBaseInit(BrandService brandService, ModelService modelService, OfferService offerService, UserService userService) {
+    public DataBaseInit(BrandService brandService, ModelService modelService, OfferService offerService, UserService userService, UserRoleService userRoleService) {
         this.brandService = brandService;
         this.modelService = modelService;
         this.offerService = offerService;
         this.userService = userService;
+        this.userRoleService = userRoleService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         brandService.initBrands();
         modelService.initModels();
+        userRoleService.initRoles();
         userService.initUsers();
         offerService.initOffers();
     }
